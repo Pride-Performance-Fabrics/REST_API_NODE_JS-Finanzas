d0567f2ffce6e6ea6046e1fd52c70f8516af9296
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2023-01-24 16:28:57 -0600

    Encriptacion de Password al iniciar sesion

--- src/routers/Usuarios/usuarioRouter.js
+++ src/routers/Usuarios/usuarioRouter.js
@@ -5,118 +5,140 @@ const querys = require("../../services/querys");
 const crypterService = require("../../services/crypto");
 const jwk = require("jsonwebtoken");
 
+const cryptoJS = require('crypto-js');
+
 
 
 // *** Obtener la lista de todos los usuarios ***
 router.get("/", async (req, res) => {
-    querys.executeQuery(
-        // `SELECT * FROM Finanzas.dbo.Users`,
-        `SELECT U.idUser, U.idPersonal, U.Usuario, U.UserName, U.Mail, 
+  querys.executeQuery(
+    // `SELECT * FROM Finanzas.dbo.Users`,
+    `SELECT U.idUser, U.idPersonal, U.Usuario, U.UserName, U.Mail, 
         R.IdRol, R.Rol, S.IdStatus, S.StatusCategory, S.StatusName
         from Finanzas.dbo.Users as U
         INNER JOIN dbo.Roles R on R.IdRol = U.IdRol
         INNER JOIN dbo.Status S on S.IdStatus = U.Status`,
-        req,
-        res
-    );
-    // console.log(res)
+    req,
+    res
+  );
+  // console.log(res)
 })
 
 // *** OPTIENE UN NUEVO TOKEN ***
 router.get("/getNewToken/", (req, res) => {
-    const tokenInfo = crypterService.verifyJWK(req);
-    if (tokenInfo.auth) {
-      res.send({ token: tokenInfo.token });
-      res.end();
-    } else {
-      res.send({
-        auth: false,
-      });
-      res.end();
-    }
-  });
+  const tokenInfo = crypterService.verifyJWK(req);
+  if (tokenInfo.auth) {
+    res.send({ token: tokenInfo.token });
+    res.end();
+  } else {
+    res.send({
+      auth: false,
+    });
+    res.end();
+  }
+});
 
 // *** VERIFICA SI EXISTE UN USUARIO *** 
 router.get("/up", async (req, res) => {
-    querys.executeQuery(
-      `select * from Finanzas.dbo.Users where idPersonal = '${req.body.idPersonal}'`,
-      req,
-      res
-    );
+  querys.executeQuery(
+    `select * from Finanzas.dbo.Users where idPersonal = '${req.body.idPersonal}'`,
+    req,
+    res
+  );
 });
 
 // *** VERIFICA EL USUARIO Y LA CONTRASENIA PARA INICIAR SESION *** 
 
 router.post("/login", async (req, res) => {
-    console.log('*****************************************************************')
-    console.log(req.body)
-    // TODO ENCRIPTAR PASSWORK
-    try {
-      const endpoint = !req.body.subscription
+  console.log('*****************************************************************')
+
+  // var encrypted = cryptoJS.AES.encrypt("Hola","finazas2023").toString()
+
+  // console.log(`Encriptacion`,encrypted)
+
+  var decrypted = cryptoJS.AES.decrypt(req.body.Password, "finazas2023").toString(cryptoJS.enc.Utf8)
+
+  // console.log(`Desencriptacion`, decrypted)
+
+  console.log(req.body)
+  // TODO ENCRIPTAR PASSWORK
+  try {
+    const endpoint = !req.body.subscription
       ? null
       : req.body.subscription.endpoint;
-      const p256dh = !req.body.subscription
-        ? null
-        : req.body.subscription.keys.p256dh;
-      const auth = !req.body.subscription
-        ? null
-        : req.body.subscription.keys.auth;
-        
-      const pool = await data.connectToDatabase();
-      const result = await pool
-        .request()
-        .input("Password", req.body.Password)
-        .input("Usuario", req.body.Usuario)
-        .input("IP", req.body.IP ?? '')
-        .execute("web.loginUserFinanzas")
-        .then(async (result) => {
-            console.log(result)
-          const user = await result.recordset[0];
+    const p256dh = !req.body.subscription
+      ? null
+      : req.body.subscription.keys.p256dh;
+    const auth = !req.body.subscription
+      ? null
+      : req.body.subscription.keys.auth;
+
+    const pool = await data.connectToDatabase();
+    const result = await pool
+      .request()
+      // .input("Password", decrypted)
+      .input("Usuario", req.body.Usuario)
+      .input("IP", req.body.IP ?? '')
+      .execute("web.loginFinanzas")
+      .then(async (result) => {
+        console.log(result)
+        const user = await result.recordset[0];
         //   console.log({...user, endpoint, p256dh, auth})
-          if (result.recordset.length > 0) {
+        if (result.recordset.length > 0) {
+
+          var Passworddecrypted = cryptoJS.AES.decrypt(result.recordset[0].PasswordN, "finazas2023").toString(cryptoJS.enc.Utf8)
+          // console.log("Passsworddecryted",Passworddecrypted)
+
+          if (Passworddecrypted === decrypted) {
             console.log("entro aqui")
             await crypterService.setToken(
               {
                 ...user,
                 subscription: !req.body.subscription
-                ? null
+                  ? null
                   : req.body.subscription,
-                },
+              },
               res
-              );
-          } else {
+            );
+          }else{
+            console.log("No existe coincidencia")
             console.log({ auth: false })
             res.send({ auth: false });
           }
-          console.log((new Date()).toLocaleString(), user)
-          console.log('*****************************************************************')
-        })
-        .catch((error) => {
-          console.log({error})
-          res.send({
-            auth: false,
-            error: error,
-          });
-          res.end();
+
+        } else {
+          console.log({ auth: false })
+          res.send({ auth: false });
+        }
+        console.log((new Date()).toLocaleString(), user)
+        console.log('*****************************************************************')
+      })
+      .catch((error) => {
+        console.log({ error })
+        res.send({
+          auth: false,
+          error: error,
         });
-  
-      res.end();
-    } catch (error) {
-      console.error(error);
-      res.send({
-        auth: false,
-        error: error,
+        res.end();
       });
-      res.end();
-    }
-  });
-  
+
+    res.end();
+  } catch (error) {
+    console.error(error);
+    res.send({
+      auth: false,
+      error: error,
+    });
+    res.end();
+  }
+});
+
 
 // *** CREAR UN NUEVO USUARIO *** 
 
 router.post("/", async (req, res) => {
-    querys.executeQuery(
-      `INSERT INTO Finanzas.dbo.Users (
+  querys.executeQuery(
+    `INSERT INTO Finanzas.dbo.Users (
                           Usuario, 
                           UserName, 
                           Password, 
@@ -131,10 +153,10 @@ router.post("/", async (req, res) => {
                           ${req.body.Status}, 
                           ${req.body.IdRol}
                           ) SELECT TOP(1) * FROM Finanzas.dbo.Users ORDER BY idUser DESC`,
-      req,
-      res
-    );
-  });
+    req,
+    res
+  );
+});
 
 
 // *** VERIFICA SI EL USUARIO ESTA AUTENTICADO *** 
