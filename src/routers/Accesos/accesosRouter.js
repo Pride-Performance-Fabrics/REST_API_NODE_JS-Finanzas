c9a83ac9fa4fe53c59b5d8a45ae29c236e92763c
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2023-02-07 13:57:13 -0600

    nuevas rutas para actualizar y los accesos de los usuario y los roles

--- src/routers/Accesos/accesosRouter.js
+++ src/routers/Accesos/accesosRouter.js
@@ -35,21 +35,21 @@ router.post("/", async (req, res) => {
 });
 
 
-router.get("/AccessGroup", async(req, res) =>{
+router.get("/AccessGroup", async (req, res) => {
     console.log("entro aqui")
     const ag = await querys.executeQuery(`SELECT * FROM Finanzas.[dbo].[vta_Accesos] order by IdAcceso ASC`, req);
 
-    if(ag.data.length > 0){
+    if (ag.data.length > 0) {
         let modulos = []
         ag.data.forEach(modulo => {
-            if(modulos.findIndex(i => i.IdMenuMenu === modulo.IdMenu)< 0){
+            if (modulos.findIndex(i => i.IdMenuMenu === modulo.IdMenu) < 0) {
                 modulos.push({
                     IdMenuMenu: modulo.IdMenu,
                     Menu: modulo.Menu,
                     URL: modulo.URL,
                     Icon: modulo.Icon,
-                    Accesos: ag.data.filter(i => i.IdMenu === modulo.IdMenu).map(i =>{
-                        return{
+                    Accesos: ag.data.filter(i => i.IdMenu === modulo.IdMenu).map(i => {
+                        return {
                             IdAcceso: i.IdAcceso,
                             Acceso: i.Acceso,
                             ActivoWeb: i.ActivoWeb,
@@ -59,12 +59,105 @@ router.get("/AccessGroup", async(req, res) =>{
                 })
             }
         });
-        res.send({data: modulos, token: req.headers['x-access-token']})
+        res.send({ data: modulos, token: req.headers['x-access-token'] })
     }
-   
+
 
     res.end()
 })
+
+// //******************** OBTENER LOS ACCESOS DE UN USUARIO ****/
+router.get('/:idUser', async (req, res) => {
+    console.log(`entro aqui en get`)
+    const resp = await querys.executeQuery(`SELECT idUser, ISNULL(Accesos, '') AS Accesos from Finanzas.dbo.Users WHERE idUser = '${req.params.idUser}'`, req);
+    console.log({ accesos: resp.data[0].Accesos })
+    if (resp.data[0].Accesos.length > 0) {
+        const numAccesos = await querys.executeQuery(`SELECT MAX(IdAcceso) as numAccesos FROM Finanzas.dbo.Accesos`, req);
+        // console.log(resp)
+        const acceso = await querys.executeQuery(`SELECT * FROM Finanzas.dbo.Accesos WHERE IdAcceso IN (${resp.data.length > 0 ? resp.data[0].Accesos : ''})`, req);
+        if (acceso.data?.length > 0 && numAccesos.data?.length > 0) {
+            // console.log('enviado')
+            res.send({
+                data: {
+                    acceso: acceso.data,
+                    numAccesos: numAccesos.data[0].numAccesos,
+                    accesos: resp.data[0].Accesos
+                },
+                token: acceso.token
+            })
+            res.end();
+        } else {
+            res.send({
+                data: {
+                    acceso: [],
+                    numAccesos: 0,
+                    accesos: ''
+                },
+                token: acceso.token
+            })
+
+            res.end();
+            console.log('no enviado', acceso, numAccesos)
+        }
+    } else {
+        res.send({
+            data: {
+                acceso: [],
+                numAccesos: 0,
+                accesos: ''
+            },
+            token: resp.token
+        })
+        res.end();
+    }
+});
+
+
+// //******************** OBTENER LOS ACCESOS DE LOS ROLES****/
+router.get('/roles/:IdRol', async (req, res) => {
+    // console.log(req.body.IdRol)
+    const resp = await querys.executeQuery(`SELECT IdRol, ISNULL(Accesos, '') AS Accesos from Finanzas.dbo.Roles WHERE IdRol = '${req.params.IdRol}'`, req);
+    console.log({ accesos: resp.data[0].Accesos })
+    if (resp.data[0].Accesos.length > 0) {
+        const numAccesos = await querys.executeQuery(`SELECT MAX(IdAcceso) as numAccesos FROM Finanzas.dbo.Accesos`, req);
+        // console.log(resp)
+        const acceso = await querys.executeQuery(`SELECT * FROM Finanzas.dbo.Accesos WHERE IdAcceso IN (${resp.data.length > 0 ? resp.data[0].Accesos : ''})`, req);
+        if (acceso.data?.length > 0 && numAccesos.data?.length > 0) {
+            // console.log('enviado')
+            res.send({
+                data: {
+                    acceso: acceso.data,
+                    numAccesos: numAccesos.data[0].numAccesos,
+                    accesos: resp.data[0].Accesos
+                },
+                token: acceso.token
+            })
+            res.end();
+        } else {
+            res.send({
+                data: {
+                    acceso: [],
+                    numAccesos: 0,
+                    accesos: ''
+                },
+                token: acceso.token
+            })
+
+            res.end();
+            console.log('no enviado', acceso, numAccesos)
+        }
+    } else {
+        res.send({
+            data: {
+                acceso: [],
+                numAccesos: 0,
+                accesos: ''
+            },
+            token: resp.token
+        })
+        res.end();
+    }
+});
 
 // //******************** CAMBIAR LOS ACCESOS DE UN USUARIO ****/
 
@@ -73,6 +166,21 @@ router.put("/cambiarAccesos", async (req, res) => {
     const resp = await querys.executeQuery(`UPDATE Finanzas.dbo.Users SET Accesos = '${req.body.accesos}' WHERE idUser = ${req.body.idUser}`, req);
     querys.executeQuery(`SELECT idUser, Accesos from Finanzas.dbo.Users WHERE idUser = ${req.body.idUser}`, req, res);
 });
+
+
+// //******************** CAMBIAR LOS ACCESOS DE LOS ROLES ****/
+
+router.put("/cambiarAccesosRoles", async (req, res) => {
+    console.log(req.body)
+    const resp = await querys.executeQuery(`UPDATE Finanzas.dbo.Roles SET Accesos = '${req.body.accesos}' WHERE IdRol = ${req.body.IdRol}`, req);
+    querys.executeQuery(`SELECT IdRol, Accesos from Finanzas.dbo.Roles WHERE IdRol = ${req.body.IdRol}`, req, res);
+});
+
+
+
+
+
+
 module.exports = router;
 
 
