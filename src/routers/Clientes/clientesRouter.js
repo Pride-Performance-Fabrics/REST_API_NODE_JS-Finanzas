4e2ae6f20de67c00205ad2d405009fb7dff284b6
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2023-03-06 08:08:00 -0600

    Consultas clientes

--- src/routers/Clientes/clientesRouter.js
+++ src/routers/Clientes/clientesRouter.js
@@ -2,27 +2,27 @@ const express = require('express');
 const router = express.Router();
 const querys = require('../../services/querys');
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
     querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_Finanzas-Customer]`, req, res);
 });
 
 
-router.get('/customerDetails', async(req, res) => {
+router.get('/customerDetails', async (req, res) => {
     querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_CustomerDetails]`, req, res);
 });
 
-router.get('/customerDetails/:IdCustomer', async(req, res) =>{
+router.get('/customerDetails/:IdCustomer', async (req, res) => {
     querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_CustomerDetails]
 	WHERE [vta_CustomerDetails].IdCustomer = ${req.params.IdCustomer}`, req, res)
 })
 
 
-router.get('/customerUltimoIdCustomer', async(req, res) =>{
+router.get('/customerUltimoIdCustomer', async (req, res) => {
     querys.executeQuery(`SELECT TOP(1) IdCustomer, CustomerName
     FROM Finanzas.dbo.Customer ORDER BY IdCustomer DESC`, req, res);
 });
 
-router.post('/IUCustomer', async(req,res) =>{
+router.post('/IUCustomer', async (req, res) => {
     console.log("-----------------------CLIENTES--------------------------")
     console.log(req.body)
     console.log("-----------------------CLIENTES--------------------------")
@@ -151,7 +151,7 @@ router.post('/IUCustomer', async(req,res) =>{
             key: "IdUserEditWeb",
             value: req.body.IdUserEditWeb
         },
-        
+
 
     ]
     querys.executeProcedure(`FINANZAS.[web].[IUCustomer]`, req, res, inputs);
@@ -159,13 +159,39 @@ router.post('/IUCustomer', async(req,res) =>{
 
 
 
+// //******************** ACTUALIZAR EL ESTADO DE LOS CHECK IsShipTO & IsBillTo ****/
+
+router.put("/checkCustomer", async (req, res) => {
+    switch (req.body.Valor) {
+        case 1:
+            const resp = await querys.executeQuery(`UPDATE Finanzas.dbo.Customer SET IsShipTo = '${req.body.Activo}' WHERE IdCustomer = ${req.body.IdCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_Finanzas-Customer]`, req, res);
+            break;
+        case 2:
+            const resp2 = await querys.executeQuery(`UPDATE Finanzas.dbo.Customer SET IsBillTo = '${req.body.Activo}' WHERE IdCustomer = ${req.body.IdCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_Finanzas-Customer]`, req, res);
+            break;
+        case 3:
+            const resp3 = await querys.executeQuery(`UPDATE Finanzas.dbo.Customer SET IsContado = '${req.body.Activo}' WHERE IdCustomer = ${req.body.IdCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_Finanzas-Customer]`, req, res);
+            break;
+
+        default:
+            const resp4 = await querys.executeQuery(`UPDATE Finanzas.dbo.Customer SET BlockFinanzas = '${req.body.Activo}' WHERE IdCustomer = ${req.body.IdCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_Finanzas-Customer]`, req, res);
+            break;
+    }
+   
+});
+
+
 //************************** CUSTOMER DETAILS */
-router.get('/customerDetailsUltimoId', async(req, res) =>{
+router.get('/customerDetailsUltimoId', async (req, res) => {
     querys.executeQuery(`SELECT TOP(1) IdDCustomer, CompanyName
     FROM [Finanzas].[dbo].[CustomerDetails] ORDER BY IdDCustomer DESC`, req, res);
 });
 
-router.post('/IUCustomerDetails', async(req,res) =>{
+router.post('/IUCustomerDetails', async (req, res) => {
     console.log("-----------------------CLIENTES Detalle--------------------------")
     console.log(req.body)
     console.log("-----------------------CLIENTES Detalle--------------------------")
@@ -190,7 +216,7 @@ router.post('/IUCustomerDetails', async(req,res) =>{
             key: "RTN",
             value: req.body.RTN
         },
- 
+
         {
             key: "CompanyName",
             value: req.body.CompanyName
@@ -243,11 +269,38 @@ router.post('/IUCustomerDetails', async(req,res) =>{
             key: "IdUserEditWeb",
             value: req.body.IdUserEditWeb
         },
-      
- 
+
+
 
     ]
     querys.executeProcedure(`[Finanzas].[web].[IUCustomerDetails]`, req, res, inputs);
+});
+
+// //******************** ACTUALIZAR EL ESTADO DE LOS CHECK IsShipTO & IsBillTo ****/
+
+router.put("/checkCustomerDetails", async (req, res) => {
+
+    switch (req.body.Valor) {
+        case 1:
+            const resp1 = await querys.executeQuery(`UPDATE Finanzas.dbo.CustomerDetails SET IsBuyer = '${req.body.Activo}' WHERE IdDCustomer = ${req.body.IdDCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_CustomerDetails]`, req, res);
+            break;
+        case 2:
+            const resp2 = await querys.executeQuery(`UPDATE Finanzas.dbo.CustomerDetails SET IsShipTo = '${req.body.Activo}' WHERE IdDCustomer = ${req.body.IdDCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_CustomerDetails]`, req, res);
+            break;
+
+        case 3:
+            const resp3 = await querys.executeQuery(`UPDATE Finanzas.dbo.CustomerDetails SET IsBillTo= '${req.body.Activo}' WHERE IdDCustomer = ${req.body.IdDCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_CustomerDetails]`, req, res);
+
+            break;
+        default:
+            const resp4 = await querys.executeQuery(`UPDATE Finanzas.dbo.CustomerDetails SET IsBillTo= '${req.body.Activo}' WHERE IdDCustomer = ${req.body.IdDCustomer}`, req);
+            querys.executeQuery(`SELECT * FROM [Finanzas].[dbo].[vta_CustomerDetails]`, req, res);
+            break;
+    }
+
 });
 
 
